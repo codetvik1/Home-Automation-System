1ec00b312673683500b806f8a4c3ac99f407a773
Ritvik Bhatia <145866042+codetvik1@users.noreply.github.com>
2024-11-02 17:33:44 +0530

    Add files via upload

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+//boolean exp for true or false
+#define ON    1
+#define OFF   0
+
+#define TEMPERATURE_GAUGE       V1
+#define COOLER_V_PIN            V0
+#define HEATER_V_PIN            V2
+#define INLET_V_PIN             V3
+#define OUTLET_V_PIN            V4
+//macros for data steams
+
+
+#endif
+
